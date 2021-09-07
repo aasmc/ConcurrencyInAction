c1d9e812c7ed9cee111a2658dd7900ebda11296e
aleksas <aasmc@yandex.ru>
2021-09-07 16:54:17 +0300

    Refactor single threaded simple queue to use dummy node to separate head and tail

--- chapter06/simple_queue.h
+++ chapter06/simple_queue.h
@@ -7,46 +7,40 @@ template<typename T>
 class simple_queue {
 private:
     struct node {
-        T data;
+        std::shared_ptr<T> data;
         std::unique_ptr<node> next;
-
-        node(T data_) : data(std::move(data_)) {}
-
     };
 
     std::unique_ptr<node> head;
     node *tail;
 
 public:
-    simple_queue() : tail(nullptr) {}
+    simple_queue() : head(new node), tail(head.get()) {}
 
     simple_queue(const simple_queue &) = delete;
 
     simple_queue &operator=(const simple_queue &) = delete;
 
     std::shared_ptr<T> try_pop() {
-        if (!head) {
+        if (head.get() == tail) {
             return std::shared_ptr<T>();
         }
         const std::shared_ptr<T> res(
-                std::make_shared<T>(std::move(head->data))
+                head->data
         );
         const std::unique_ptr<node> old_head = std::move(head);
         head = std::move(old_head->next);
-        if (!head) {
-            tail = nullptr;
-        }
         return res;
     }
 
     void push(T new_value) {
-        std::unique_ptr<node> p(new node(std::move(new_value)));
+        std::shared_ptr<T> new_data(
+                std::make_shared<T>(std::move(new_value))
+        );
+        std::unique_ptr<node> p(new node);
+        tail->data = new_data;
         node *const new_tail = p.get();
-        if (tail) {
-            tail->next = std::move(p);
-        } else { // this means it is the first element in the queue
-            head = std::move(p);
-        }
+        tail->next = std::move(p);
         tail = new_tail;
     }
 
